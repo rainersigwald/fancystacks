1a2d46160ab90854b0fd3f06cef3693b5a90b757
Rainer Sigwald <raines@microsoft.com>
2021-05-18 13:57:08 -0500

    Parse with a regex

--- fancystacks.test/UnitTest1.cs
+++ fancystacks.test/UnitTest1.cs
@@ -10,9 +10,11 @@ namespace fancystacks.test
         public void TestMethod1()
         {
             string markdown = fancystacks.Program.Markdownify(@"Microsoft.Build.dll!Microsoft.Build.Execution.TaskRegistry.RegisteredTaskRecord.GetTaskFactory(targetLoggingContext, elementLocation, taskProjectFile) Line 1295
-	at S:\msbuild\src\Build\Instance\TaskRegistry.cs(1295)", "https://github.com/dotnet/msbuild/blob/4242f381a90f77f483b123819759db1ff8eddf67/");
+	at S:\msbuild\src\Build\Instance\TaskRegistry.cs(1295)", 
+    "https://github.com/dotnet/msbuild/blob/4242f381a90f77f483b123819759db1ff8eddf67/",
+    @"S:\msbuild");
 
-            markdown.ShouldBe(@"* [`Microsoft.Build.dll!Microsoft.Build.Execution.TaskRegistry.RegisteredTaskRecord.GetTaskFactory(targetLoggingContext, elementLocation, taskProjectFile) Line 1295`](https://github.com/dotnet/msbuild/blob/4242f381a90f77f483b123819759db1ff8eddf67/src/Build/Instance/TaskRegistry.cs#L1295)
+            markdown.ShouldBe(@"* [`Microsoft.Build.dll!Microsoft.Build.Execution.TaskRegistry.RegisteredTaskRecord.GetTaskFactory(targetLoggingContext, elementLocation, taskProjectFile)`](https://github.com/dotnet/msbuild/blob/4242f381a90f77f483b123819759db1ff8eddf67/src/Build/Instance/TaskRegistry.cs#L1295)
 ");
         }
     }
--- fancystacks/Program.cs
+++ fancystacks/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace fancystacks
 {
     public class Program
     {
+        static Regex parser = new(
+            @"^(?<method>.*\)) Line \d+\r?\n\s+at (?<file>[^(]+)\((?<line>\d+)\)",
+            RegexOptions.Multiline | RegexOptions.Compiled);
+
         static void Main(string[] args)
         {
             // Read stack from clipboard or prompt.
@@ -16,22 +21,37 @@ namespace fancystacks
             // copy to clipboard
         }
 
-        public static string Markdownify(string stack, string baseUri)
+        public static string Markdownify(string stack, string baseUri, string repoRoot)
         {
+            if (!repoRoot.EndsWith('\\'))
+            {
+                repoRoot += '\\';
+            }
+
             StringBuilder o = new(stack.Length);
 
-            int i = stack.IndexOf('\n');
+            foreach (Match match in parser.Matches(stack))
+            {
+                GroupCollection groups = match.Groups;
+                string file = groups["file"].Value;
 
-            o.Append("* [`");
-            o.Append(stack, 0, i);
-
-            System.Console.WriteLine(o.ToString());
-
-            o.Append("`](asdf");
-
-            System.Console.WriteLine(o.ToString());
-
-            // o.Append(baseUri);
+                if (file.StartsWith(repoRoot))
+                {
+                    o.Append("* [`");
+                    o.Append(groups["method"]);
+                    o.Append("`](");
+                    o.Append(baseUri);
+                    o.Append(file.Substring(repoRoot.Length).Replace('\\', '/'));
+                    o.Append("#L");
+                    o.Append(groups["line"]);
+                    o.AppendLine(")");
+                }
+                else
+                {
+                    o.Append("* ");
+                    o.AppendLine(groups["method"].Value);
+                }
+            }
 
             return o.ToString();
         }
