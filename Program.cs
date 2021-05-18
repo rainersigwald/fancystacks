3f98477adfe055abe88e5265b91774f1adcb4888
Rainer Sigwald <raines@microsoft.com>
2021-05-18 15:25:15 +0000

    Project skeleton

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fancystacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read stack from clipboard or prompt.
+
+            // Get base URL including commit
+
+            // transform into markdown
+
+            // copy to clipboard
+        }
+    }
+}
